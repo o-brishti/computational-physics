172c34ea5c2fb6dec826fed61a6fa7a74280d155
ashen <69895907+o-brishti@users.noreply.github.com>
2024-06-12 20:39:03 +0530

    eulershm.py

--- eulershm.py
+++ eulershm.py
@@ -1,13 +1,17 @@
+#simulating the motion of a damped harmonic oscillator using the euler method
 from matplotlib import pyplot as plt
 
-def f(x,t):			#m*(d2x/dt2)
+#define a function f(x, t) representing the force acting on the body
+def f(x,t):			
 	return -(k*x)
-	
+
+#initial conditions for position x0, velocity v0, and time t0
 x0 = 10 
-v0 = 0		#dx/dt
+v0 = 0		
 
 a = 0
 
+#define the final time tf and the time step dt
 t0 = 0
 tf = 20
 dt = 0.01
@@ -19,10 +23,12 @@ t = t0
 k = 1
 m = 2
 
+#initialize lists to store positions, velocities, and times
 xlist = [x]
 vlist = [v]
 tlist = [t]
 
+#calculate number of steps
 n = int((tf - t0)/dt)
 
 #euler method implementation
